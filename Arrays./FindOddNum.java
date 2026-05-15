9ccd4a7e9f1b9149b4b95eaf405129e83c194c86
vansh <vanshdadwal49@gmail.com>
2026-05-16 02:07:08 +0530

    Update and rename TwoSum.java to FindOddNum.java

---
+++ Arrays./FindOddNum.java
@@ -0,0 +1,10 @@
+public class arr1 {
+    public static void main(String[] args) {
+        int arr[]={2,7,8,11,14,15};
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+                System.out.println("odd:-"+arr[i]);
+            }
+        }
+    }
+}
