da1dd10de9ad28b36a617a6464f6902cf367c492
vansh <vanshdadwal49@gmail.com>
2026-05-16 03:06:06 +0530

    add min element finder using arrays
    Java program to find the minimum element in an array using loop and comparison logic.

---
+++ Arrays./FindMin.java
@@ -0,0 +1,13 @@
+public class arr4 {
+    public static void main(String[] args) {
+        int arr[]={5,2,18,7,11};
+        int min = arr[0];
+        for(int i =0;i<arr.length;i++){
+            if (arr[i]<min){
+                min = arr[i];
+            }
+        }
+        System.out.println("min is-:"+min);
+
+    }
+}
