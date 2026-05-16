8242ee02c2d962d69f8e8d46585c32a9edcabf88
vansh <vanshdadwal49@gmail.com>
2026-05-16 21:42:38 +0530

    Add ReverseArraySwap class to reverse an array
    Java program to reverse an array using swapping with start and end pointers.

---
+++ Arrays./ReverseArraySwap.java
@@ -0,0 +1,17 @@
+public class arr9 {
+    public static void main(String[] args) {
+        int arr[] = {2,3,54,76,877};
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            int temp = arr[start];
+            arr[start]= arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
